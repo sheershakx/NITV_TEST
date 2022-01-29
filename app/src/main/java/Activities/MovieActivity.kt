7b01490943e6aab2ce037d2c522a7c1d1b2b4c31
Sheershak Raj Gautam <sheershak.gautam123@gmail.com>
2022-01-29 23:10:08 +0545

    Updated- Internet Connection Check
    Assessment Completed

--- app/src/main/java/Activities/MovieActivity.kt
+++ app/src/main/java/Activities/MovieActivity.kt
@@ -4,24 +4,16 @@ import Adapters.movieInfoAdapter
 import LocalStorage.DatabaseHelper
 import Models.MovieModel
 import RetrofitFiles.MovieAPI
-import android.app.ProgressDialog
 import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.widget.GridLayout
-import android.widget.ProgressBar
 import android.widget.Toast
-import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.thex.nitv_test.R
 import com.thex.nitv_test.databinding.ActivityMovieBinding
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.Retrofit
 
 class MovieActivity : AppCompatActivity() {
     private var api_key: String = "80dff2970093b6849866a98cc4bf6f34"
@@ -39,6 +31,11 @@ class MovieActivity : AppCompatActivity() {
         //set header text of activity through layout  header
         binding.headerlayout.tvHeaderText.text = "Movies List"
         binding.rvMovieList.layoutManager = GridLayoutManager(applicationContext, 3)
+
+       if ( !CheckInternet().checkForInternet(applicationContext))
+        {
+            Toast.makeText(applicationContext, "Please Check Internet Connection !!", Toast.LENGTH_SHORT).show()
+        }
         //fetch data from retrofit || localDB
         getLocalRecord()
         fetchData()
